10ed805487cfb7c5511aa753e5aaac17bd7d8fa8
lephilong190702 <lephilong1907@gmail.com>
2023-11-06 11:38:46 +0700

    Update database

--- app/dao.py
+++ app/dao.py
@@ -1,71 +1,13 @@
+from app.models import Category, Product
+
+
 def get_categories():
-    return [{
-        "id": 1,
-        "name": "Mobile"
-    }, {
-        "id":2,
-        "name": "Tablet"
-    }]
+    return Category.query.all()
 
 
 def get_products(kw):
-    products = [{
-        "id": 1,
-        "name": "Iphone 15",
-        "price": 25000000,
-        "image": "https://res.cloudinary.com/dxxwcby8l/image/upload/v1688179242/hclq65mc6so7vdrbp7hz.jpg",
-        "category_id": 1
-    }, {
-        "id": 2,
-        "name": "Iphone 15 Pro",
-        "price": 30000000,
-        "image": "https://res.cloudinary.com/dxxwcby8l/image/upload/v1688179242/hclq65mc6so7vdrbp7hz.jpg",
-        "category_id": 1
-    }, {
-        "id": 3,
-        "name": "Oppo",
-        "price": 20000000,
-        "image": "https://res.cloudinary.com/dxxwcby8l/image/upload/v1688179242/hclq65mc6so7vdrbp7hz.jpg",
-        "category_id": 1
-    }, {
-        "id": 4,
-        "name": "Samsung Galaxy S1",
-        "price": 15000000,
-        "image": "https://res.cloudinary.com/dxxwcby8l/image/upload/v1688179242/hclq65mc6so7vdrbp7hz.jpg",
-        "category_id": 1
-    }, {
-        "id": 5,
-        "name": "Samsung Galaxy S5",
-        "price": 23000000,
-        "image": "https://res.cloudinary.com/dxxwcby8l/image/upload/v1688179242/hclq65mc6so7vdrbp7hz.jpg",
-        "category_id": 1
-    }, {
-        "id": 6,
-        "name": "Realme",
-        "price": 15000000,
-        "image": "https://res.cloudinary.com/dxxwcby8l/image/upload/v1688179242/hclq65mc6so7vdrbp7hz.jpg",
-        "category_id": 1
-    }, {
-        "id": 7,
-        "name": "Xiaomi 13 ",
-        "price": 17000000,
-        "image": "https://res.cloudinary.com/dxxwcby8l/image/upload/v1688179242/hclq65mc6so7vdrbp7hz.jpg",
-        "category_id": 1
-    }, {
-        "id": 8,
-        "name": "Xiaomi 13 Pro",
-        "price": 20000000,
-        "image": "https://res.cloudinary.com/dxxwcby8l/image/upload/v1688179242/hclq65mc6so7vdrbp7hz.jpg",
-        "category_id": 1
-    }, {
-        "id": 9,
-        "name": "OnePlus Ace",
-        "price": 10000000,
-        "image": "https://res.cloudinary.com/dxxwcby8l/image/upload/v1688179242/hclq65mc6so7vdrbp7hz.jpg",
-        "category_id": 1
-    }]
+    products = Product.query
 
     if kw:
-        products = [p for p in products if p['name'].find(kw) >= 0]
-
-    return products;
+        products = products.filter(Product.name.contains(kw))
+    return products.all();
