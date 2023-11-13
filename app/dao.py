cd881c37722ad68543065b398fb2cf805a313d81
lephilong1907@gmail.com <lephilong1907@gmail.com>
2023-11-13 11:31:32 +0700

    Update admin page

--- app/dao.py
+++ app/dao.py
@@ -1,4 +1,4 @@
-from app.models import Category, Product
+from app.models import Category, Product, User
 
 
 def get_categories():
@@ -11,3 +11,7 @@ def get_products(kw):
     if kw:
         products = products.filter(Product.name.contains(kw))
     return products.all();
+
+
+def get_user_by_id(user_id):
+    return User.query.get(user_id)
